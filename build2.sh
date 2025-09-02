ea0dccc35add308850331c6bf7a88e38d9236ff1
Matteo Costantini <matteo.costantini@gmail.com>
2025-09-02 17:06:44 +0200

    Add missing include for cstdint in build script

--- build2.sh
+++ build2.sh
@@ -34,7 +34,7 @@
         ws=$(ls external/TinyWebsockets/tiny_websockets_lib/src/*.cpp)
         otf=$(ls external/OpenThings-Framework-Firmware-Library/*.cpp)
         ifx=$(ls external/influxdb-cpp/*.hpp)
-        g++ -o OpenSprinkler -DOSPI $USEGPIO $ADS1115 $PCF8591 -DSMTP_OPENSSL $DEBUG -std=c++17 -include string.h main.cpp \
+        g++ -o OpenSprinkler -DOSPI $USEGPIO $ADS1115 $PCF8591 -DSMTP_OPENSSL $DEBUG -std=c++17 -include string.h -include cstdint main.cpp \
                 OpenSprinkler.cpp program.cpp opensprinkler_server.cpp utils.cpp weather.cpp gpio.cpp mqtt.cpp sunrise.cpp \
                 smtp.c RCSwitch.cpp sensor*.cpp notifier.cpp naett.c \
                 $ADS1115FILES $PCF8591FILES \
